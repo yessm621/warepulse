b0e411d8f0cad92c34739cfd2595de1d622c1ab4
yessm621 <yessm621@gmail.com>
2025-12-23 15:26:18 +0900

    feat: 추가된 domain에 대한 repository 작성

---
+++ src/main/java/com/me/warepulse/adjustment/AdjustmentRepository.java
@@ -0,0 +1,6 @@
+package com.me.warepulse.adjustment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdjustmentRepository extends JpaRepository<Adjustment, Long> {
+}
