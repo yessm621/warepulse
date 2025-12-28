0dd6d119011787d56a7b5498fdcdf9547767778c
yessm621 <yessm621@gmail.com>
2025-12-28 22:54:34 +0900

    fix: Jpa Auditing 오류 수정
    - Jpa Auditing인 createdAt, updatedAt, createdBy, updatedBy가 계속 Null인 버그 발견
    - UserAuditor -> AuditorAwareImpl로 변경
    - createdBy, updatedBy를 Long -> String으로 변경
    - JpaConfig 설정을 통해 Jpa Auditing 동작

--- src/main/java/com/me/warepulse/utils/BaseEntity.java
+++ src/main/java/com/me/warepulse/utils/BaseEntity.java
@@ -3,6 +3,7 @@ package com.me.warepulse.utils;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
