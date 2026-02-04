34d00609f63a692a3b70f5ddd1ca7844b6fbcad0
yessm621 <yessm621@gmail.com>
2026-02-04 22:45:38 +0900

    fix: JpaAuditing 오류 수정

---
+++ inventory-service/src/main/java/com/me/inventoryservice/config/AuditorAwareImpl.java
@@ -0,0 +1,27 @@
+package com.me.inventoryservice.config;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null ||
+                !authentication.isAuthenticated() ||
+                authentication.getPrincipal().equals("anonymousUser")) {
+            return Optional.empty();
+        }
+
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        return Optional.of(userDetails.getUsername());
+    }
+}
---
+++ inventory-service/src/main/java/com/me/inventoryservice/config/GatewayHeaderFilter.java
@@ -0,0 +1,35 @@
+package com.me.inventoryservice.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+public class GatewayHeaderFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String username = request.getHeader("X-User-Name");
+        String role = request.getHeader("X-User-Role");
+
+        if (username != null) {
+            List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(role));
+
+            CustomUserDetails userDetails = new CustomUserDetails(username, authorities);
+
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
