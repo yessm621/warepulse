255343675f70f2c63861d013f0f8b024232dab85
yessm621 <yessm621@gmail.com>
2026-02-06 16:28:55 +0900

    feat: adjustment-service init
    - adjustment-service 초기 셋팅
    - root 프로젝트에 adjustment-service 모듈 추가

---
+++ adjustment-service/src/main/java/com/me/adjustmentservice/AdjustmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.me.adjustmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdjustmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdjustmentServiceApplication.class, args);
+    }
+
+}
---
+++ adjustment-service/src/test/java/com/me/adjustmentservice/AdjustmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.adjustmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdjustmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
